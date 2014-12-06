615257139749c6971f2b26bb7aaf025628e9cd5d
Stephen Wiley <swiley@Stephen-Wileys-MacBook-Pro-2.local>
2014-12-06 02:18:01 +0000

    add a crap ton of math functions, still doesnt work though...

--- game.c
+++ game.c
@@ -1,18 +1,68 @@
 #include "game.h"
 
 struct vect2
-mulvect(struct vec2 a, struct vec2 b)
+mulvect(struct vect2 a, struct vect2 b)
 {
-	a.y=a.y*b.y;
-	return a;
+	return (struct vect2){.x=a.x*b.x,.y=a.y*b.y};//TODO: unfinished
+}
+struct vect2 
+mulvectscal(struct vect2 vect, float scal)
+{
+	return (struct vect2){.x=vect.x*scal,.y=vect.y*scal}; 
+}
+struct vect2
+addvect(struct vect2 a, struct vect2 b)
+{
+	return (struct vect2){.x=a.x+b.x, .y=a.y+b.y};
+}
+struct vect2
+atovect(float a)
+{
+	return (struct vect2){
+		.x=cos(atan(a)),
+		.y=sin(atan(a))
+	};
+}
+float
+mag(struct vect2 v)
+{
+	return v.x+v.y;
+}
+float
+nderiv( float (*f)(float), float x, float h)
+{
+	return (f(x+h)-f(x-h));	
+}
+
+struct vect2
+colground()
+{
+	return (struct vect2){.x=0,.y=ship1.pos.y<terainf(ship1.pos.x) ? -1 : 0};
+}
+void
+applyforce(struct mass *ob, struct force f)
+{
+	//f=ma
+	//f/m=a
+	//TODO:moments
+	ob->vel=addvect(ob->vel,f.w);
+}
+void
+applyforcecenter(struct mass *ob, struct vect2 f)
+{
+	ob->vel=addvect(ob->vel,f);
 }
 void
 logic(void)
 {
 	ship1.vel.y=ship1.vel.y+0.1;
-	if(colground())
+	if(colground().y+colground().x)
 	{
-		ship.vel=mulvect(ship.vel,colground());
+		applyforcecenter(&ship1, 
+			mulvectscal(atovect(nderiv(terainf,0.1,ship1.pos.x)),
+				mag(ship1.vel)*-2
+			)
+		);
 	}
 	ship1.pos.x=ship1.pos.x+ship1.vel.x;
 	ship1.pos.y=ship1.pos.y+ship1.vel.y;
--- game.h
+++ game.h
@@ -1,18 +1,29 @@
 #include <SDL/SDL.h>
 #include "SDL/SDL_opengl.h"
 #include <math.h>
-struct vect2
-{
+#define vec2 vect2
+//I keep typoing vect2 as vec2 
+float terainf(float x);
+
+struct vect2 {
 	float x,y;
-}
+};
+struct force {
+	struct vect2 pos;
+	struct vect2 w;
+};
+
 struct mass
 {
-	vect2.pos;
-	vect2.vel;
+	float mass; //<- kind of important
+	float elasticity;
+	struct vect2 pos;
+	struct vect2 vel;
 	float r,vr; //rotation (speed)
 	float rm[10]; //varius submases that can be jetosoned to cause a rection force on the object
 	float imp[10]; //impulse of mass being jetosoned
 	//TODO:   ^that's retarded
 	//TODO: change center of gravity when mass is jetosoned
 } ship1;
+
 
